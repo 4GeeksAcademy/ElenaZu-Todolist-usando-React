77a8807bd13a50c6afcc58fb989806c8e597da5d
ElenaZu <128751514+ElenaZu@users.noreply.github.com>
2023-07-18 00:18:47 +0000

    FuncionalidadesTodoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [textInput, setTextInput] = useState("");
+	const [taskList, setTaskList] = useState([]);
+
+	function handleChange(e) {
+		setTextInput(e.target.value);
+	}
+
+	function handleTask(e) {
+		if (e.key == "Enter" && textInput !== "") {
+			setTaskList([...taskList, textInput]);
+			setTextInput("");
+		}
+	}
+
+	function deleteTask(position) {
+		setTaskList(taskList.filter((_, index) => index !== position))
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1 className="text-center">TODOS</h1>
+			<div className='container'>
+				<div className="input-icon"><input type="text" placeholder="What needs to be done" value={textInput} onChange={handleChange} onKeyDown={handleTask} /></div>
+				{
+					taskList.map((task, index) => {
+						return (
+							<div className="input-icon">
+
+								<input key={index} value={task} />
+								<FontAwesomeIcon className="icon" icon={faTimes} onClick={() => { deleteTask(index) }} />
+							</div>
+						)
+					})
+				}
+			</div>
+			{
+				<div className="footer">
+				<h5>
+					{
+						taskList.length
+					}
+					items left
+				</h5>
+				</div>
+			}
 		</div>
 	);
 };
